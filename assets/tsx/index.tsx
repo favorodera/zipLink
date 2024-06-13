c40eadd3446eee56661e0a75e9ee95071e240bf3
Favour Chidera Emeka <favorodera@gmail.com>
2024-06-13 16:37:39 +0100

    first commit

---
+++ assets/tsx/index.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Index() {
+    return (
+        <>
+        <p>HELLO</p>
+        </>
+    )
+}
+
+export default Index
---
+++ assets/tsx/render.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM  from 'react-dom/client'
+import Index from '../tsx/index.tsx'
+import '../scss/index.scss'
+
+const app = ReactDOM.createRoot(document.querySelector('.root-container')!)
+app.render(
+    <React.StrictMode>
+        <Index />
+    </React.StrictMode>
+)
