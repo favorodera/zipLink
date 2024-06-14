5d3f17472a622beb0bacaabea9952cedea453b3a
Favour Chidera Emeka <favorodera@gmail.com>
2024-06-14 10:39:18 +0100

    setup hero section

---
+++ assets/tsx/components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Hero() {
+    return (
+        <section className="hero-container">
+            <div className="hero-intro">
+            <h1>Shorten Your Loooong Links :)</h1>
+            <p>ZipLink is an efficient and easy-to-use URL shortening service that streamlines your online experience.</p>
+            </div>
+        </section>
+    )
+}
--- assets/tsx/index.tsx
+++ assets/tsx/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Nav from "./components/Nav";
+import Hero from "./components/Hero";
 
 function Index() {
   return (
     <>
-      <Nav></Nav>
+      <Nav />
+      <div>
+        <Hero />
+      </div>
     </>
   );
 }
