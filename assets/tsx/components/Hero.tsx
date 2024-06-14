7756d5f887600b384f5a59747163ef3d5c90befe
Favour Chidera Emeka <favorodera@gmail.com>
2024-06-14 15:05:34 +0100

    update

--- assets/tsx/components/Hero.tsx
+++ assets/tsx/components/Hero.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 
 export default function Hero() {
-    return (
-        <section className="hero-container">
-            <div className="hero-intro">
-            <h1>Shorten Your Loooong Links :)</h1>
-            <p>ZipLink is an efficient and easy-to-use URL shortening service that streamlines your online experience.</p>
-            </div>
-        </section>
-    )
+  return (
+    <section className="hero-container">
+      <div className="hero-intro">
+        <h1>Shorten Your Loooong Links :)</h1>
+        <p>
+          ZipLink is an efficient and easy-to-use URL shortening service that
+          streamlines your online experience.
+        </p>
+      </div>
+    </section>
+  );
 }
